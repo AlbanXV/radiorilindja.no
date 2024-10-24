6e7cff011a531a595745934640cd5411f2874722
Alban Bajrami <albanba@uio.no>
2024-10-25 00:29:18 +0200

    Changed bg video

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import rr_bg from '../assets/rr_bg.mp4'
+import kujdesi_promo from '../assets/kujdesi.mp4'
 
 function VideoSection() {
   return (
     <div className='select-none mt-[-50px] mb-8 w-full h-full flex-col justify-center flex'>
         <video autoPlay loop muted>
-            <source src={rr_bg} type='video/mp4' />
+            <source src={kujdesi_promo} type='video/mp4' />
         </video>
     </div>
   )
