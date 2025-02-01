c08484636d0acaa6dbe1046de9f54f8eb89b7c5d
Alban Bajrami <albanba@uio.no>
2025-02-01 17:07:57 +0100

    replaced image

--- src/components/SupportSection.js
+++ src/components/SupportSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Vipps from '../assets/vipps.png';
-import gras from '../assets/gras.jpg';
+import gras from '../assets/gras.jpeg';
 
 function SupportSection() {
 
@@ -14,11 +14,11 @@ function SupportSection() {
         <div className='flex justify-center space-x-4 py-6'>
           
           <div>
-            <img className='rounded-md' src={Vipps} />
+            <img className='rounded-md w-[460px]' src={Vipps} />
           </div>
 
           <div>
-            <Link to='https://www.norsk-tipping.no/grasrotandelen/din-mottaker/926943545'><img className='w-[291px] rounded-md' src={gras} /></Link>
+            <Link to='https://www.norsk-tipping.no/grasrotandelen/din-mottaker/926943545'><img className='rounded-md' src={gras} /></Link>
           </div>
           
         </div>
